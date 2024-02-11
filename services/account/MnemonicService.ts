f9c5304f7a88cc4f3ce85418cc381ac94b4287b8
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2024-02-12 08:20:04 +0900

    アカウントの作成とストレージへの書き込み機能を追加

--- services/account/MnemonicService.ts
+++ services/account/MnemonicService.ts
@@ -1,5 +1,9 @@
-import { InvalidValueError } from '@/models/ErrorModels';
+import { MnemonicModel } from '@/models/AccountModel';
+import { InvalidValueError, StorageError } from '@/models/ErrorModels';
+import { SecureStorage } from '@/util/storages/SecureStorage';
 import { MnemonicPassPhrase, ExtendedKey, Wallet, Network } from 'symbol-hd-wallets';
+
+const SECURE_STORAGE_KEY: string = 'MNEMONIC_SERVICE';
 
 export class MnemonicService {
   public readonly privateKey: string;
@@ -23,20 +27,50 @@ export class MnemonicService {
     return wallet.getChildAccountPrivateKey(`m/44'/4343'/0'/0'/${height}'`);
   }
 
-  static createRandom(): MnemonicService {
+  public static createRandom(): MnemonicService {
     const mnemonic = MnemonicPassPhrase.createRandom();
     const seed = mnemonic.toSeed().toString('hex');
     return new MnemonicService(mnemonic.plain, seed);
   }
 
-  static generateFromPhrase(mnemonicPhrase: string): MnemonicService {
-    const mnemonic = new MnemonicPassPhrase(mnemonicPhrase);
+  /** ローカルストレージに保存されたニーモニックを返却する */
+  public static async getFromStorage() {
+    const storage = new SecureStorage(SECURE_STORAGE_KEY);
+    const item = await storage.getItem();
+    if (!item) {
+      throw new StorageError('Failed to read from storage.');
+    }
+    return JSON.parse(item) as MnemonicModel;
+  }
 
+  public static generateFromPhrase(mnemonicPhrase: string): MnemonicService {
+    const mnemonic = new MnemonicPassPhrase(mnemonicPhrase);
     if (!mnemonic.isValid()) {
       throw new InvalidValueError('Incorrect mnemonic format');
     }
-
     const seed = mnemonic.toSeed().toString('hex');
     return new MnemonicService(mnemonic.plain, seed);
+  }
+
+  /** ニーモニックをストレージへ保存する。デバイスにつきニーモニックは1つとする */
+  public async replaceToStorage(): Promise<void> {
+    try {
+      const storage = new SecureStorage(SECURE_STORAGE_KEY);
+      const item = JSON.stringify({ mnemonic: this.mnemonic } as MnemonicModel);
+      await storage.setItem(item);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  /** ニーモニックに紐づくアドレスを指定した height まで取得する */
+  public getChildAddresses(start: number = 0, end: number = 30): string[] {
+    const xkey = ExtendedKey.createFromSeed(this.seed, Network.SYMBOL);
+    const wallet = new Wallet(xkey);
+    let addresses: string[] = [];
+    for (let i = start; i <= end; i++) {
+      addresses.push(wallet.getChildAccountPrivateKey(`m/44'/4343'/0'/0'/${i}'`));
+    }
+    return addresses;
   }
 }
