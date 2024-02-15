8037b9292db4446082e47bd7806f5bfb2a23ee8f
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2024-02-15 12:28:36 +0900

    add hooks

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,7 @@
 import { View, Text } from 'react-native';
 import { Link } from 'expo-router';
-import { useLoadWallets } from '@/hooks/useLoadWallets';
 
 export default function Root(): React.JSX.Element {
-  const { isLoading, error, wallets } = useLoadWallets();
-
-  console.log(isLoading, error, wallets);
-
   return (
     <View className='flex-1 justify-center items-center gap-3'>
       <Text className='font-bold'>wallet 一覧と情報の表示</Text>
